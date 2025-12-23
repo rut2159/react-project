f91d5bfe69625b4bef63a9388e31a5482df37ff5
sari8508776 <sari8508776@gmail.com>
2025-12-23 23:16:28 +0200

    add componnent by sari

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import NewComponent from './components/NewComponent';
 
 function App() {
   return (
     <div className="App">
       <MyComponent />
+      <div style={{ marginTop: 20 }}>
+        <NewComponent
+          title={"A friendly new component"}
+          onAction={(msg) => console.log('NewComponent action:', msg)}
+        />
+      </div>
     </div>
   );
 }
