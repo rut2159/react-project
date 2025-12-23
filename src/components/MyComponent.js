4736630215503f7e377da66db54e6986002b0e1a
tovi1892 <tovi1892@gmail.com>
2025-12-23 21:29:42 +0200

    add commponent

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>זהו הקומפוננטה החדשה שלי!</h1>
+      <p>ברוכים הבאים לפרויקט React.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
