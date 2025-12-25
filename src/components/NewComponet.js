81ae98cc5932a0fc62cc1119ffeb57f1f16fc7c8
rut2159 <ruti4269@gmail.com>
2025-12-25 15:07:11 +0200

    add new componet of ruti

---
+++ src/components/NewComponet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>זהו הקומפוננטה החדשה שלי!</h1>
+      <p>בהצלחה בפרויקט </p>
+    </div>
+  );
+}
+
+export default MyComponent;
