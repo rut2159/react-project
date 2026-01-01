dcfb98f18ff2410770ccf58db0d5e643a490762f
tovi1892 <tovi1892@gmail.com>
2026-01-01 14:02:03 +0200

    delede navigate component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,5 +1,4 @@
 import EventForm from "./EventFrom.jsx";
-import Navigation from "./Navigation.jsx";
 import "./Home.css";
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
@@ -17,13 +16,12 @@ function Home() {
       <div className="home">
         <h1>הזנת פרטי השבת הנוכחית: </h1>
         <EventForm />
-        <Navigation />
       </div>
 
-      <div style={{ marginTop: 12 }}>
-        <button onClick={goToCookings}>הצגת התבשילים (עמוד)</button>
-        <button onClick={goToHomeTasks} style={{ marginLeft: 8 }}>משימות בית (עמוד)</button>
-        <button onClick={goToShopping} style={{ marginLeft: 8 }}>קניות (עמוד)</button>
+      <div >
+        <button onClick={goToCookings}>הצגת התבשילים </button>
+        <button onClick={goToHomeTasks} >משימות בית </button>
+        <button onClick={goToShopping} >קניות </button>
       </div>
     </>
   );
