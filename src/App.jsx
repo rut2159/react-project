86fcf141be2f0021d707bd7d8ab4f020e614a9be
tamaR-797 <3295797@gmail.com>
2026-02-27 10:14:18 +0200

    delete context

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,19 @@ import Home from "./Home/Home.jsx";
 import HomeTasks from './HomeTasks/homeTasks.jsx';
 import CookingComponent from './Cooking/CookingComponent.jsx';
 import {ShoppingList} from './ShoppingList/ShoppingList.jsx';
-import { GlobalProvider } from './context/GlobalContext.jsx';
+
 
 function App() {
   return (
     <>
-      <GlobalProvider>
+     
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home-tasks" element={<HomeTasks />} />
           <Route path="/cookings" element={<CookingComponent />} />
           <Route path="/shopping" element={<ShoppingList />} />
         </Routes>
-      </GlobalProvider>
+ 
     </>
   )
 }
